5f452c6718de53dff7529e7c474be9888fe0d229
pedrobarbosa0 <47837747+pedrobarbosa0@users.noreply.github.com>
2021-12-19 20:54:54 -0300

    feature(meta tags): add meta tags and refactor some images

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,15 +4,39 @@ export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="pt-br">
+				<title>Pedro Barbosa</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<Head>
 					<meta charSet="UTF-8" />
-					<meta name="description" content="Pedro Barbosa" />
+					<meta name="author" content="Pedro Barbosa" />
+					<meta
+						name="keywords"
+						content="#react #nextsjs #front-end #frontend #developer #desenvolvedor # typescript #javascript"
+					></meta>
+					<meta name="description" content="Portifolio Pedro Barbosa" />
 					<link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="true" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
 						rel="stylesheet"
 					/>
+					<link rel="shortcut icon" href="/web.svg" type="image/x-icon" />
+
+					{/* Twitter */}
+					<meta name="twitter:card" content="summary_large_image" />
+					<meta name="twitter:title" content="⚡ Pedro Barbosa" />
+					<meta name="twitter:description" content="Portifolio Pedro Barbosa" />
+					<meta name="twitter:site" content="@" />
+					<meta name="twitter:creator" content="@" />
+					<meta name="twitter:image" content="/web.svg" />
+
+					{/* Open Graph general (Facebook, Pinterest) */}
+					<meta property="og:title" content="⚡ Pedro Barbosa" />
+					<meta property="og:description" content="Portifolio Pedro Barbosa" />
+					<meta property="og:url" content="https://pedroab.com" />
+					<meta property="og:site_name" content="pedroab.com" />
+					<meta property="og:type" content="website" />
+					<meta property="og:image" content="/web.svg" />
 				</Head>
 				<body>
 					<Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,12 +34,10 @@ const Home: NextPage = () => {
 				<section className={styles.about}>
 					<h2>Sobre mim</h2>
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-						non proident
+						Um dev front-end que sonha com React e seu ecossistema quase todas as
+						noites. Movido a desafios, então se tem um problema ou um projeto para
+						implementar e precisa de um dev ReactJS, me chama no Linkedin ou manda um
+						e-mail que vai ser um prazer ajudar.
 					</p>
 				</section>
 
